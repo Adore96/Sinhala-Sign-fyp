e419bea81b491713b75c4ae3da621e37a3617a79
Kalhara Perera <adorekasun@gmail.com>
2020-06-16 03:19:23 +0530

    Added Image Augmentation

--- VGGNet/smallervggnet.py
+++ VGGNet/smallervggnet.py
@@ -8,12 +8,26 @@ from keras.layers.core import Flatten
 from keras.layers.core import Dropout
 from keras.layers.core import Dense
 from keras import backend as K
-
+from keras.preprocessing.image import ImageDataGenerator
 
 
 class SmallerVGGNet:
+
+
+
 	@staticmethod
 	def build(width, height, depth, classes):
+
+#added traindatagen and test data gen functions
+		traindata_gen = ImageDataGenerator(
+			rescale=1/255,
+			shear_range=0.2,
+			zoom_range=0.2,
+			horizontal_flip=True
+		)
+
+		testdata_gen = ImageDataGenerator(rescale=1/255)
+
 		# initialize the model along with the input shape to be
 		# "channels last" and the channels dimension itself
 		model = Sequential()
