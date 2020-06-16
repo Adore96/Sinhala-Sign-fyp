eb1c24ba3ee9b48f5869c58fcd90806f821aef0a
Kalhara Perera <adorekasun@gmail.com>
2020-06-16 13:30:38 +0530

    Layers Updated

--- VGGNet/smallervggnet.py
+++ VGGNet/smallervggnet.py
@@ -41,36 +41,38 @@ class SmallerVGGNet:
 			inputShape = (depth, height, width)
 			chanDim = 1
 
-		# CONV => RELU => POOL
+		# CONV => RELU => POOL   1st Layer
 		model.add(Conv2D(32, (3, 3), padding="same", input_shape=inputShape))
 		model.add(Activation("relu"))
 		model.add(BatchNormalization(axis=chanDim))
 		model.add(MaxPooling2D(pool_size=(3, 3)))
 		model.add(Dropout(0.25)) #can remove this line if want used it to remove overfitting
 
-		# (CONV => RELU) * 2 => POOL
+		# (CONV => RELU) * 2 => POOL	2st Layer
 		model.add(Conv2D(64, (3, 3), padding="same"))
 		model.add(Activation("relu"))
 		model.add(BatchNormalization(axis=chanDim))
 
+		#3st Layer
 		model.add(Conv2D(64, (3, 3), padding="same"))
 		model.add(Activation("relu"))
 		model.add(BatchNormalization(axis=chanDim))
 		model.add(MaxPooling2D(pool_size=(2, 2)))
 		model.add(Dropout(0.25))
 
-		# (CONV => RELU) * 2 => POOL
+		# (CONV => RELU) * 2 => POOL	4st Layer
 		model.add(Conv2D(128, (3, 3), padding="same"))
 		model.add(Activation("relu"))
 		model.add(BatchNormalization(axis=chanDim))
 
+			#5st Layer
 		model.add(Conv2D(128, (3, 3), padding="same"))
 		model.add(Activation("relu"))
 		model.add(BatchNormalization(axis=chanDim))
 		model.add(MaxPooling2D(pool_size=(2, 2)))
 		model.add(Dropout(0.25))
 
-		# first (and only) set of FC => RELU layers
+		# first (and only) set of FC => RELU layers		Output Layer
 		model.add(Flatten())
 		model.add(Dense(1024))
 		model.add(Activation("relu"))
